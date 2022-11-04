f8f7b71ea49c9d31ddd9430fe31fdfbe4418f4a4
mtaufiikh <mtaufiikh@gmail.com>
2022-11-04 10:01:27 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -362,10 +362,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
     </script>
-    <script src="{{asset('js/app.js')}}"></script>
+
     <script
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJfzOqR9u2eyXv6OaiuExD3jzoBGGIVKY&libraries=places&callback=myMap">
     </script>
+
+    @include('script')
 </body>
 
 </html>

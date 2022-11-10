274f50da6ee92a1672171ff0d4ce510b7d960b92
yusufstawan <yusufsetiyawan45@gmail.com>
2022-11-10 13:26:19 +0700

    slicing form pendaftaran

--- resources/views/form-pendaftaran.blade.php
+++ resources/views/form-pendaftaran.blade.php
@@ -12,6 +12,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/css/dropify.min.css" />
 </head>
 
 <body>
@@ -34,8 +35,97 @@
     </div>
 
 
-    
+    <!-- header form -->
+    <div class="container text-center mt-5">
+        <h1 class="form-header_header">Daftarkan Bengkel Anda</h1>
+        <p class="form-header_text">Daftarkan bengkel anda di KEBANAN sekarang juga untuk menjangkau lebih banyak
+            pelanggan.</p>
+    </div>
 
+    <!-- form -->
+    <form class="container container-form_mobile w-75 mx-auto mt-5">
+        <div class="row gap-5 form-row_mobile">
+            <div class="col mb-4">
+                <label class="form-label">Nama Bengkel <span class="text-danger">*</span> </label>
+                <input type="text" class="form-control" placeholder="Masukkan nama bengkel..." required />
+            </div>
+            <div class="col mb-4">
+                <label class="form-label">Nama Pemilik <span class="text-danger">*</span> </label>
+                <input type="text" class="form-control" placeholder="Masukan nama pemilik..." required />
+            </div>
+        </div>
+        <div class="row gap-5 form-row_mobile">
+            <div class="col mb-4">
+                <label class="form-label">No HP <span class="text-danger">*</span> </label>
+                <input type="text" class="form-control" placeholder="Masukan no HP..." required />
+            </div>
+            <div class="col mb-4">
+                <label class="form-label">Keterangan <span class="text-danger">*</span> </label>
+                <input type="text" class="form-control" placeholder="Masukan keterangan bengkel anda..." required />
+            </div>
+        </div>
+        <div class="row gap-5">
+            <div class="col mb-4">
+                <label class="form-label">Alamat <span class="text-danger">*</span> </label>
+                <input type="text" class="form-control" placeholder="Masukan alamat bengkel anda..." required />
+            </div>
+        </div>
+
+        <!-- map -->
+        <p class="form-map_caution">Pilih/klik titik lokasi bengkel anda di map <img
+                src="{{ asset('images/warning.svg') }}" alt="warning" width="9px" height="9px" /></p>
+        <div class="map-container mb-4">
+            <div id="googleMap" class="form-map"></div>
+        </div>
+
+        <div class="row gap-5 form-row_mobile">
+            <div class="col mb-4">
+                <label class="form-label">Latitude</label>
+                <input type="text" class="form-control" />
+            </div>
+            <div class="col mb-4">
+                <label class="form-label">Longitude </label>
+                <input type="text" class="form-control" />
+            </div>
+        </div>
+
+        <div class="row gap-5 form-row_mobile">
+            <div class="col mb-4">
+                <label class="form-label">Foto Bengkel <span class="text-danger">*</span> </label>
+                <input type="file" class="dropify d-block" data-height="300" />
+            </div>
+            <div class="col mb-4">
+                <label class="form-label">Jenis Layanan <span class="text-danger">*</span> </label>
+                <div class="form-check">
+                    <div class="d-flex align-items-end mb-2">
+                        <input class="form-check-input" type="checkbox" value="" />
+                        <label class="form-check-label">Terima Tubles</label>
+                    </div>
+                    <div class="d-flex align-items-end mb-2">
+                        <input class="form-check-input" type="checkbox" value="" />
+                        <label class="form-check-label">Terima non Tubles</label>
+                    </div>
+                    <div class="d-flex align-items-end mb-2">
+                        <input class="form-check-input" type="checkbox" value="" />
+                        <label class="form-check-label">Terima Panggilan</label>
+                    </div>
+                    <div class="d-flex align-items-end mb-2">
+                        <input class="form-check-input" type="checkbox" value="" />
+                        <label class="form-check-label">Terima Motor</label>
+                    </div>
+                    <div class="d-flex align-items-end mb-2">
+                        <input class="form-check-input" type="checkbox" value="" />
+                        <label class="form-check-label">Terima Mobil</label>
+                    </div>
+                    <div class="d-flex align-items-end">
+                        <input class="form-check-input" type="checkbox" value="" />
+                        <label class="form-check-label">Terima Kendaraan Berat</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button type="submit" class="button-green mx-auto form-button mb-5">Daftar Sekarang</button>
+    </form>
 
     <!-- footer -->
     <div class="container d-flex align-items-baseline mb-4 gap-5 footer">
@@ -106,6 +196,7 @@
     </nav>
 
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/js/dropify.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
 
@@ -113,6 +204,11 @@
 
     <script
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBj_bRjxZDlmaH8c0e0qKIpUTFF05g1nd0&libraries=places&callback=myMap">
+    </script>
+    <script>
+        $(document).ready(function() {
+            $(".dropify").dropify();
+        });
     </script>
 
 </body>
